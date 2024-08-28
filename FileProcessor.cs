75e38bebad59d77dc6f68e58a77436de95ce2059
romansechin <romansechin@gmail.com>
2024-08-28 15:37:47 +0300

    Добавьте файлы проекта.

---
+++ FileProcessor.cs
@@ -0,0 +1,12 @@
+﻿using static System.Console;
+
+namespace DataProcessorApp;
+
+internal class FileProcessor
+{
+    public string InputFilePath {  get; }
+    public FileProcessor (string inputFilePath) => InputFilePath = inputFilePath;
+    public void Process() {
+        Console.WriteLine($"Begin process in {InputFilePath}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using DataProcessorApp;
+using static System.Console;
+
+WriteLine("Parsing command line options");
+
+var command = args[0];
+
+if (command == "--file")
+{
+    var filePath = args[1];
+    WriteLine($"Single file {filePath} selected");
+    ProcessSingleFile(filePath);
+}
+else if (command == "--dir")
+{
+    var directoryPath = args[1];
+    var fileType = args[2];
+    WriteLine($"Directory {directoryPath} selected for {fileType} files");
+    ProcessDirectory(directoryPath, fileType);
+}
+else {
+    WriteLine("Invalid command line options");
+}
+
+WriteLine("Press Enter to quit");
+ReadLine();
+
+static void ProcessSingleFile(string filePath) { 
+    var fileProcessor = new FileProcessor(filePath);
+    fileProcessor.Process();
+}
+
+static void ProcessDirectory(string directoryPath, string fileType) { 
+    
+}
